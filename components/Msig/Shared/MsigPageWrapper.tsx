b93b2463e49d658a783c9c30f67ccdef9766a535
Thijs van Hoof <thijs.v.h@gmail.com>
2022-02-23 22:11:05 +0100

    Simplify MSIG page wrapper

--- components/Msig/Shared/MsigPageWrapper.tsx
+++ components/Msig/Shared/MsigPageWrapper.tsx
@@ -30,62 +30,52 @@ const MsigPageWrapper = ({
     navigate(router, { pageUrl: PAGE.NODE_DISCONNECTED })
   }, [router])
 
+  if (hideNav) {
+    return cloneElement(children, props)
+  }
+
   return (
     <Box
+      position='relative'
       display='flex'
       justifyContent='center'
       width='100%'
-      minHeight='100vh'
       p={3}
+      // padding for logout button to ensure it never sits on top of the content
+      paddingBottom={8}
     >
-      {hideNav ? (
-        cloneElement(children, props)
-      ) : (
-        <>
-          <Box
-            position='relative'
-            display='flex'
-            justifyContent='center'
-            width='100%'
-            p={3}
-            // padding for logout button to ensure it never sits on top of the content
-            paddingBottom={8}
-          >
-            {msig.loading && <LoadingScreen width='100%' />}
-            {!msig.loading && (
-              <Box display='flex' flexDirection='column' width='100%'>
-                <PageHeader
-                  msigAddress={msig.Address}
-                  walletAddress={wallet.address}
-                />
-                {cloneElement(children, props)}
-              </Box>
-            )}
-
-            <ButtonV2
-              css={`
-                position: absolute;
-                bottom: 0;
-                left: 0;
-              `}
-              m={5}
-              onClick={resetWallet}
-            >
-              Logout
-            </ButtonV2>
-            <NetworkConnection
-              position='absolute'
-              bottom='0'
-              right='0'
-              m={5}
-              lotusApiAddr={process.env.NEXT_PUBLIC_LOTUS_NODE_JSONRPC}
-              apiKey={process.env.NEXT_PUBLIC_NODE_STATUS_API_KEY}
-              statusApiAddr={process.env.NEXT_PUBLIC_NODE_STATUS_API_ADDRESS}
-              errorCallback={onNodeDisconnect}
-            />
-          </Box>
-        </>
+      {msig.loading && <LoadingScreen width='100%' />}
+      {!msig.loading && (
+        <Box display='flex' flexDirection='column' width='100%'>
+          <PageHeader
+            msigAddress={msig.Address}
+            walletAddress={wallet.address}
+          />
+          {cloneElement(children, props)}
+        </Box>
       )}
+
+      <ButtonV2
+        css={`
+          position: absolute;
+          bottom: 0;
+          left: 0;
+        `}
+        m={5}
+        onClick={resetWallet}
+      >
+        Logout
+      </ButtonV2>
+      <NetworkConnection
+        position='absolute'
+        bottom='0'
+        right='0'
+        m={5}
+        lotusApiAddr={process.env.NEXT_PUBLIC_LOTUS_NODE_JSONRPC}
+        apiKey={process.env.NEXT_PUBLIC_NODE_STATUS_API_KEY}
+        statusApiAddr={process.env.NEXT_PUBLIC_NODE_STATUS_API_ADDRESS}
+        errorCallback={onNodeDisconnect}
+      />
     </Box>
   )
 }
