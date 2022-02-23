1ea2e3de79dcb8b72707e0e34bfbceed0337ea74
Thijs van Hoof <thijs.v.h@gmail.com>
2022-02-23 18:56:56 +0100

    Update create msig page

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback } from 'react'
 import { useRouter } from 'next/router'
 import { RequireWallet } from '@glif/wallet-provider-react'
+import { Page, OneColumnCentered } from '@glif/react-components'
 import CreateMsig from '../../components/Msig/Create'
-import { MsigPageWrapper } from '../../components/Msig/Shared'
 import { navigate } from '../../utils/urlParams'
 import { PAGE } from '../../constants'
 
@@ -13,11 +13,19 @@ const Create = () => {
     [router]
   )
   return (
-    <RequireWallet gatekeep={gatekeep}>
-      <MsigPageWrapper hideNav>
-        <CreateMsig />
-      </MsigPageWrapper>
-    </RequireWallet>
+    <Page
+      back={gatekeep}
+      homeUrl={process.env.NEXT_PUBLIC_HOME_URL}
+      blogUrl={process.env.NEXT_PUBLIC_BLOG_URL}
+      walletUrl={process.env.NEXT_PUBLIC_WALLET_URL}
+      explorerUrl={process.env.NEXT_PUBLIC_EXPLORER_URL}
+    >
+      <OneColumnCentered>
+        <RequireWallet gatekeep={gatekeep}>
+          <CreateMsig />
+        </RequireWallet>
+      </OneColumnCentered>
+    </Page>
   )
 }
 
