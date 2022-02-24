ad3b5091429b4d30aa9da814315f317bea4a6a6b
Thijs van Hoof <thijs.v.h@gmail.com>
2022-02-25 00:30:16 +0100

    Cleanup MSIG wrapper and page header

--- components/Msig/Shared/MsigPageWrapper.tsx
+++ components/Msig/Shared/MsigPageWrapper.tsx
@@ -1,17 +1,9 @@
-import React, { cloneElement, ReactElement, useCallback } from 'react'
+import React, { cloneElement, ReactElement } from 'react'
 import { node } from 'prop-types'
-import {
-  Box,
-  LoadingScreen,
-  ButtonV2,
-  NetworkConnection
-} from '@glif/react-components'
+import { Box, LoadingScreen } from '@glif/react-components'
 import { useWallet } from '@glif/wallet-provider-react'
-import { useRouter } from 'next/router'
 
 import { useMsig } from '../../../MsigProvider'
-import { resetWallet, navigate } from '../../../utils/urlParams'
-import { PAGE } from '../../../constants'
 import PageHeader from './PageHeader'
 
 const MsigPageWrapper = ({
@@ -22,11 +14,6 @@ const MsigPageWrapper = ({
 }) => {
   const msig = useMsig()
   const wallet = useWallet()
-  const router = useRouter()
-
-  const onNodeDisconnect = useCallback(() => {
-    navigate(router, { pageUrl: PAGE.NODE_DISCONNECTED })
-  }, [router])
 
   return (
     <Box
@@ -45,28 +32,6 @@ const MsigPageWrapper = ({
           {cloneElement(children, props)}
         </Box>
       )}
-
-      <ButtonV2
-        css={`
-          position: absolute;
-          bottom: 0;
-          left: 0;
-        `}
-        m={5}
-        onClick={resetWallet}
-      >
-        Logout
-      </ButtonV2>
-      <NetworkConnection
-        position='absolute'
-        bottom='0'
-        right='0'
-        m={5}
-        lotusApiAddr={process.env.NEXT_PUBLIC_LOTUS_NODE_JSONRPC}
-        apiKey={process.env.NEXT_PUBLIC_NODE_STATUS_API_KEY}
-        statusApiAddr={process.env.NEXT_PUBLIC_NODE_STATUS_API_ADDRESS}
-        errorCallback={onNodeDisconnect}
-      />
     </Box>
   )
 }
--- components/Msig/Shared/PageHeader.tsx
+++ components/Msig/Shared/PageHeader.tsx
@@ -1,16 +1,9 @@
-import { useCallback } from 'react'
 import styled from 'styled-components'
-import { useRouter } from 'next/router'
 import {
-  NavLink,
   Box,
-  Title,
-  IconGlif,
   Label,
   CopyText
 } from '@glif/react-components'
-import { PAGE } from '../../../constants'
-import { generateRouteWithRequiredUrlParams } from '../../../utils/urlParams'
 import { ADDRESS_PROPTYPE } from '../../../customPropTypes'
 import truncateAddress from '../../../utils/truncateAddress'
 
@@ -59,86 +52,10 @@ const PageHeader = ({
   msigAddress: string
   walletAddress: string
 }) => {
-  const router = useRouter()
-  const getRoute = useCallback(generateRouteWithRequiredUrlParams, [
-    router.query
-  ])
-
   return (
-    <Box mb={6}>
-      <Box position='absolute'>
-        <Box display='flex' alignItems='center'>
-          <IconGlif
-            size={6}
-            css={`
-              transform: rotate(-90deg);
-            `}
-          />
-          <Title ml={2}>Safe</Title>
-        </Box>
-      </Box>
-      <Box
-        display='flex'
-        alignItems='center'
-        flexDirection='row'
-        justifyContent='space-between'
-        mb={3}
-      >
-        <Box
-          display='flex'
-          alignItems='flex-start'
-          flexDirection='row'
-          mx='auto'
-        >
-          <NavLink
-            href={getRoute({
-              existingQParams: router.query as Record<string, string>,
-              pageUrl: PAGE.MSIG_HOME
-            })}
-            isActive={router.pathname === PAGE.MSIG_HOME}
-            mr={3}
-          >
-            Assets
-          </NavLink>
-          <NavLink
-            href={getRoute({
-              existingQParams: router.query as Record<string, string>,
-              pageUrl: PAGE.MSIG_HISTORY
-            })}
-            isActive={router.pathname === PAGE.MSIG_HISTORY}
-            mr={3}
-          >
-            History
-          </NavLink>
-          <NavLink
-            href={getRoute({
-              existingQParams: router.query as Record<string, string>,
-              pageUrl: PAGE.MSIG_PROPOSALS
-            })}
-            isActive={
-              router.pathname === PAGE.MSIG_PROPOSALS ||
-              router.pathname.includes(PAGE.MSIG_PROPOSAL)
-            }
-            mr={3}
-          >
-            Proposals
-          </NavLink>
-          <NavLink
-            href={getRoute({
-              existingQParams: router.query as Record<string, string>,
-              pageUrl: PAGE.MSIG_ADMIN
-            })}
-            isActive={router.pathname === PAGE.MSIG_ADMIN}
-            mr={3}
-          >
-            Admin
-          </NavLink>
-          <Box display='flex' flexDirection='row' mt={1}>
-            <Address label='Safe Address' address={msigAddress} />
-            <Address label='Wallet Address' address={walletAddress} />
-          </Box>
-        </Box>
-      </Box>
+    <Box display='flex' flexDirection='row' mt={1} mb={3}>
+      <Address label='Safe Address' address={msigAddress} />
+      <Address label='Wallet Address' address={walletAddress} />
     </Box>
   )
 }
