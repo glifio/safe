7a5774e1b3d2fcd498d81bfdd6ec46bdda009698
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-02 16:32:44 +0100

    Remove MsigPageWrapper and PageHeader

--- components/Msig/Shared/index.ts
+++ components/Msig/Shared/index.ts
@@ -1,4 +1,3 @@
 export * from './Headers'
-export { default as MsigPageWrapper } from './MsigPageWrapper'
 export { default as Signers } from './Signers'
 export { default as Address } from './Address'
