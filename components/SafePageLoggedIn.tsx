ad89958dd8c4004155f75acfd6e1f050ec183708
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-03 11:40:40 +0100

    Update addressLinks property name

--- components/SafePageLoggedIn.tsx
+++ components/SafePageLoggedIn.tsx
@@ -37,7 +37,7 @@ export default function SafePageLoggedIn(props: SafePageLoggedInProps) {
           errorCallback={onNodeDisconnect}
         />
       }
-      addresses={[
+      addressLinks={[
         {
           label: 'Safe Address',
           address: msig.Address,
