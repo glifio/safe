7010f4f82ec155906432628fa59c1c1b1d06d7e5
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-02 14:53:48 +0200

    approve cancel

--- pages/approve.tsx
+++ pages/approve.tsx
@@ -1,27 +1,27 @@
 import { useCallback } from 'react'
 import { useRouter } from 'next/router'
 import { OneColumnCentered, RequireWallet } from '@glif/react-components'
+
 import SafePageLoggedIn from '../components/SafePageLoggedIn'
-import ApproveReject from '../components/Msig/ApproveCancel'
+import { ApproveCancel, ApproveCancelMode } from '../components/Msig/ApproveCancel'
 import { navigate } from '../utils/urlParams'
 import { PAGE } from '../constants'
 
-const Approve = () => {
+const ApprovePage = () => {
   const router = useRouter()
   const gatekeep = useCallback(
     () => navigate(router, { pageUrl: PAGE.LANDING }),
     [router]
   )
-
   return (
     <SafePageLoggedIn>
       <OneColumnCentered>
         <RequireWallet gatekeep={gatekeep}>
-          <ApproveReject />
+          <ApproveCancel mode={ApproveCancelMode.APPROVE} />
         </RequireWallet>
       </OneColumnCentered>
     </SafePageLoggedIn>
   )
 }
 
-export default Approve
+export default ApprovePage
--- pages/cancel.tsx
+++ pages/cancel.tsx
@@ -1,27 +1,27 @@
 import { useCallback } from 'react'
 import { useRouter } from 'next/router'
 import { OneColumnCentered, RequireWallet } from '@glif/react-components'
+
 import SafePageLoggedIn from '../components/SafePageLoggedIn'
-import ApproveCancel from '../components/Msig/ApproveCancel'
+import { ApproveCancel, ApproveCancelMode } from '../components/Msig/ApproveCancel'
 import { navigate } from '../utils/urlParams'
 import { PAGE } from '../constants'
 
-const Cancel = () => {
+const CancelPage = () => {
   const router = useRouter()
   const gatekeep = useCallback(
     () => navigate(router, { pageUrl: PAGE.LANDING }),
     [router]
   )
-
   return (
     <SafePageLoggedIn>
       <OneColumnCentered>
         <RequireWallet gatekeep={gatekeep}>
-          <ApproveCancel />
+          <ApproveCancel mode={ApproveCancelMode.CANCEL} />
         </RequireWallet>
       </OneColumnCentered>
     </SafePageLoggedIn>
   )
 }
 
-export default Cancel
+export default CancelPage
