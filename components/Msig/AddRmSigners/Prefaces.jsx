d3938056b4dd55e27218c29ca32f08d347ef2606
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-02 11:21:45 +0200

    Update Prefaces.jsx

--- components/Msig/AddRmSigners/Prefaces.jsx
+++ components/Msig/AddRmSigners/Prefaces.jsx
@@ -4,15 +4,6 @@ import { Warning } from '@glif/react-components'
 const Preface = ({ method }) => {
   return (
     <>
-      {method === 5 && (
-        <Warning
-          title='Warning'
-          description={[
-            "You're about to add another signer to your Safe.",
-            'Please make sure you know and trust the new owner as they will be able to withdraw funds from your Safe.'
-          ]}
-        />
-      )}
       {method === 6 && (
         <Warning
           title='Warning'
