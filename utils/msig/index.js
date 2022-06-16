d87d37090fa795d50153734bfea00a85c4e3b933
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-16 13:50:36 +0200

    Delete fetchAndSetMsigActor

--- utils/msig/index.js
+++ utils/msig/index.js
@@ -1,3 +1,2 @@
 export { default as isAddressSigner } from './isAddressSigner'
 export { default as fetchMsigState } from './fetchMsigState'
-export { default as fetchAndSetMsigActor } from './fetchAndSetMsigActor'
