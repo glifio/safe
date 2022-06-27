a3d5affc7c5c5c050c6e0dbc8cda287da1b067f0
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-27 14:44:20 +0200

    Update use-desktop-browser.test.js

--- pages-test/error/use-desktop-browser.test.js
+++ pages-test/error/use-desktop-browser.test.js
@@ -1,6 +1,6 @@
-import { render, screen, act, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
-import UseDesktopBrowser from '../../pages/error/use-desktop-browser.jsx'
+import UseDesktopBrowser from '../../pages/error/use-desktop-browser'
 import composeMockAppTree from '../../test-utils/composeMockAppTree'
 
 jest.mock('@glif/filecoin-wallet-provider')
@@ -13,42 +13,15 @@ describe('UseDesktopBrowser', () => {
         <UseDesktopBrowser />
       </Tree>
     )
-
-    expect(container.firstChild).toMatchSnapshot()
-  })
-
-  test('it renders the message about getting Chrome', () => {
-    const { Tree } = composeMockAppTree('postOnboard')
-
-    render(
-      <Tree>
-        <UseDesktopBrowser />
-      </Tree>
-    )
+    
+    expect(
+      screen.getByText('Glif Safe isn’t ready for your phone or tablet.')
+    ).toBeInTheDocument()
 
     expect(
-      screen.getByText('Glif Wallet isn’t ready for your phone or tablet.')
-    ).toBeInTheDocument()
-  })
+      screen.getByText('Home')
+    ).toHaveAttribute('href', 'https://glif.io')
 
-  test.skip('it sends the user to glif home page after clicking Home', async () => {
-    const { Tree } = composeMockAppTree('preOnboard')
-    delete window.location
-
-    window.location = {
-      href: ''
-    }
-
-    await act(async () => {
-      render(
-        <Tree>
-          <UseDesktopBrowser />
-        </Tree>
-      )
-      fireEvent.click(screen.getByText('Home'))
-      jest.runAllTimers()
-    })
-
-    expect(window.location.href).toBe('https://www.glif.io')
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
