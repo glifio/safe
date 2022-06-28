4fb2563e7152aaf43c6342db8b4541b9f41e00dd
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-28 11:02:19 +0200

    Fix useMsig loading state

--- MsigProvider/index.tsx
+++ MsigProvider/index.tsx
@@ -1,4 +1,11 @@
-import { useState, createContext, ReactNode, useContext, Dispatch } from 'react'
+import {
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+  ReactNode,
+  Dispatch
+} from 'react'
 import { useWallet } from '@glif/react-components'
 import { FilecoinNumber } from '@glif/filecoin-number'
 import useSWR from 'swr'
@@ -8,8 +15,9 @@ import { MsigActorState, emptyMsigState } from './types'
 
 const MsigProviderContext = createContext<
   MsigActorState & {
+    loading: boolean
     setMsigActor: null | Dispatch<string | null>
-  } & { loading: boolean }
+  }
 >({
   ...emptyMsigState,
   loading: true,
@@ -28,11 +36,18 @@ export const MsigProviderWrapper = ({
   test: boolean
 }) => {
   const wallet = useWallet()
+  const [loading, setLoading] = useState<boolean>(false)
   const [msigActor, setMsigActor] = useState(null)
   const { data: actor, error: msigActorStateError } = useSWR(
     msigActor ? [msigActor, wallet.address] : null,
     fetchMsigState
   )
+
+  // Set loading to true when changing the msigActor
+  useEffect(() => msigActor && setLoading(true), [msigActor])
+
+  // Set loading to false after fetching new data
+  useEffect(() => actor && setLoading(false), [actor])
 
   return (
     <MsigProviderContext.Provider
@@ -42,7 +57,6 @@ export const MsigProviderWrapper = ({
         Balance: actor?.Balance || new FilecoinNumber('0', 'fil'),
         AvailableBalance:
           actor?.AvailableBalance || new FilecoinNumber('0', 'fil'),
-        loading: !actor,
         Signers: actor?.Signers || [],
         NextTxnID: actor?.NextTxnID || 0,
         NumApprovalsThreshold: actor?.NumApprovalsThreshold || 0,
@@ -55,6 +69,7 @@ export const MsigProviderWrapper = ({
           actorNotFound: false,
           unhandledError: msigActorStateError ? msigActorStateError : ''
         },
+        loading,
         setMsigActor
       }}
     >
