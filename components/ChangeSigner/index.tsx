6d5d02c19e6898ba15827beee7bb3a2fe3a102a7
Thijs van Hoof <thijs.v.h@gmail.com>
2022-07-08 13:10:00 +0200

    Ensure old and new signer validity in ChangeSigner

--- components/ChangeSigner/index.tsx
+++ components/ChangeSigner/index.tsx
@@ -53,8 +53,10 @@ export const ChangeSigner = ({
   const message = useMemo<Message | null>(() => {
     try {
       // Manually check signer validity to prevent passing invalid addresses to serializeParams.
-      // This can happen due to multiple rerenders when using setIsValid from InputV2.Address.
-      return validateAddressString(newSigner)
+      // This can happen due to multiple rerenders when using setIsValid from InputV2.Address
+      // or InputV2.Select resetting the value when it's not present in the options.
+      return validateAddressString(oldSigner) &&
+        validateAddressString(newSigner)
         ? new Message({
             to: Address,
             from: wallet.robust,
