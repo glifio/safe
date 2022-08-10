67420fcf97b1efb17b1b3db4f077573909f46cc8
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-10 15:49:37 +0200

    Update admin page

--- components/Admin/index.tsx
+++ components/Admin/index.tsx
@@ -5,7 +5,6 @@ import {
   PageTitle,
   useWallet,
   navigate,
-  ButtonV2,
   ButtonRowCenter,
   WideDialog,
   LoadingScreen
@@ -15,19 +14,18 @@ import { PAGE } from '../../constants'
 import { useMsig } from '../../MsigProvider'
 import { SignersTable } from './SignersTable'
 
-const TitleRow = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 3em;
-  h3 {
-    margin-top: 0;
-    flex-grow: 1;
+const RequiredApprovals = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
-`
 
-const Info = styled.p`
-  color: var(--gray-medium);
-  margin-top: 0;
+  p {
+    color: var(--gray-medium);
+    margin-top: 0;
+    padding-left: 1em;
+  }
 `
 
 export default function Owners() {
@@ -42,50 +40,42 @@ export default function Owners() {
       <PageTitle>Safe Admin</PageTitle>
       <hr />
       <WideDialog>
+        <RequiredApprovals>
+          <div>
+            <h3>Required Approvals: {NumApprovalsThreshold}</h3>
+            <ButtonV2Link href={PAGE.MSIG_CHANGE_APPROVALS}>Edit</ButtonV2Link>
+          </div>
+          <p>
+            The number of approvals required for a Safe proposal to execute.
+          </p>
+        </RequiredApprovals>
 
-        <TitleRow>
-          <h3>Required Approvals: {NumApprovalsThreshold}</h3>
-          <ButtonV2Link href={PAGE.MSIG_CHANGE_APPROVALS}>Edit</ButtonV2Link>
-        </TitleRow>
-        
-        <Info>
-          The number of approvals required for a Safe proposal to execute.
-        </Info>
-
-        <h3>Signer Addresses</h3>
-
-        <SignersTable
-          signers={signers}
-          wallet={wallet}
-          onRemove={(address: string) => {
-            navigate(router, {
-              pageUrl: PAGE.MSIG_REMOVE_SIGNER,
-              params: {
-                address
-              }
-            })
-          }}
-          onChange={(address: string) => {
-            navigate(router, {
-              pageUrl: PAGE.MSIG_CHANGE_SIGNER,
-              params: {
-                address
-              }
-            })
-          }}
-        />
-
-        <ButtonRowCenter>
-          <ButtonV2
-            onClick={() =>
+        <div>
+          <h3>Signer Addresses</h3>
+          <SignersTable
+            signers={signers}
+            wallet={wallet}
+            onRemove={(address: string) => {
               navigate(router, {
-                pageUrl: PAGE.MSIG_ADD_SIGNER
+                pageUrl: PAGE.MSIG_REMOVE_SIGNER,
+                params: {
+                  address
+                }
               })
-            }
-          >
-            Add signer
-          </ButtonV2>
-        </ButtonRowCenter>
+            }}
+            onChange={(address: string) => {
+              navigate(router, {
+                pageUrl: PAGE.MSIG_CHANGE_SIGNER,
+                params: {
+                  address
+                }
+              })
+            }}
+          />
+          <ButtonRowCenter>
+            <ButtonV2Link href={PAGE.MSIG_ADD_SIGNER}>Add signer</ButtonV2Link>
+          </ButtonRowCenter>
+        </div>
       </WideDialog>
     </div>
   )
