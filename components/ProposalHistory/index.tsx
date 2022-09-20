d1aecf4de0898e15b21134056a6a2be609acf269
Thijs van Hoof <thijs.v.h@gmail.com>
2022-09-20 11:08:49 +0200

    Get proposal by id

--- components/ProposalHistory/index.tsx
+++ components/ProposalHistory/index.tsx
@@ -1,25 +1,14 @@
 import {
   getQueryParam,
-  navigate,
   appendQueryParams,
   ProposalDetail,
   ProposalHistoryTable,
-  MsigTransaction,
   useWallet
 } from '@glif/react-components'
 import { useRouter } from 'next/router'
-import cloneDeep from 'lodash.clonedeep'
 
 import { useMsig } from '../../MsigProvider'
 import { PAGE } from '../../constants'
-
-const getProposalParam = (proposal: MsigTransaction): string => {
-  const clone = cloneDeep(proposal)
-  delete clone.__typename
-  delete clone.to.__typename
-  clone.approved.forEach((a) => delete a.__typename)
-  return encodeURI(JSON.stringify(clone))
-}
 
 const ProposalHistory = () => {
   const { Address } = useMsig()
@@ -27,39 +16,21 @@ const ProposalHistory = () => {
   const router = useRouter()
   const id = getQueryParam.number(router, 'id')
 
-  const approve = (proposal: MsigTransaction) => {
-    navigate(router, {
-      pageUrl: PAGE.MSIG_APPROVE,
-      params: {
-        proposal: getProposalParam(proposal)
-      }
-    })
-  }
-
-  const cancel = (proposal: MsigTransaction) => {
-    navigate(router, {
-      pageUrl: PAGE.MSIG_CANCEL,
-      params: {
-        proposal: getProposalParam(proposal)
-      }
-    })
-  }
-
   return !isNaN(id) && id >= 0 ? (
     <ProposalDetail
       id={id}
       msigAddress={Address}
       walletAddress={wallet}
-      approve={approve}
-      cancel={cancel}
+      approve={(id: number) => appendQueryParams(PAGE.MSIG_APPROVE, { id })}
+      cancel={(id: number) => appendQueryParams(PAGE.MSIG_CANCEL, { id })}
     />
   ) : (
     <ProposalHistoryTable
       msigAddress={Address}
       walletAddress={wallet}
       idHref={(id: number) => appendQueryParams(PAGE.MSIG_PROPOSAL, { id })}
-      approve={approve}
-      cancel={cancel}
+      approve={(id: number) => appendQueryParams(PAGE.MSIG_APPROVE, { id })}
+      cancel={(id: number) => appendQueryParams(PAGE.MSIG_CANCEL, { id })}
     />
   )
 }
